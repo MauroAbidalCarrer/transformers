d853008312f0592bbeb44c9b8fc6dc9d4568e6f8
Mauro Abidal Carrer <mauroabidal@yahoo.fr>
2025-09-17 17:42:49 +0000

    WIP: Reimplementing Adnrej Karpathy's GPT.

--- yes.py
+++ yes.py
@@ -1,14 +1,18 @@
+from math import sqrt
 from functools import partial
 
 import torch
 from torch import nn
 from torch import Tensor
-from torch.nn import functional
+from torch.nn import functional as F
 
 TEST_SPLIT_RATIO = 0.1
 ATTENTION_WINDOW_SIZE = 256
 BATCH_SIZE = 64
-
+N_EMBEDING_DIMS = 384
+LEARNING_RATE = 1e-3
+N_TRAINING_STEPS = 5000
+LOGGING_INTERVAL = 500
 
 # ! wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
 
@@ -25,7 +29,7 @@ decode = lambda tokens_idx: "".join([token_idx_to_str[token_idx] for token_idx i
 encoded_txt = encode(shakespeare_txt)
 yes = lambda obj: isinstance(obj, int)
 
-dataset = torch.tensor(encoded_txt, dtype=torch.float)
+dataset = torch.tensor(encoded_txt, dtype=torch.long)
 n_test_samples = int(TEST_SPLIT_RATIO * len(shakespeare_txt))
 train = dataset[:n_test_samples]
 test = dataset[n_test_samples:]
@@ -33,13 +37,117 @@ test = dataset[n_test_samples:]
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
-def get_random_batch(split: Tensor) -> Tensor:
-    rand_idx = torch.randint(high=len(split), size=(BATCH_SIZE, ))
+def get_random_batch(split: Tensor) -> tuple[Tensor, Tensor]:
+    rand_idx = torch.randint(high=len(split - ATTENTION_WINDOW_SIZE), size=(BATCH_SIZE, ))
     x = torch.stack([split[idx:idx + ATTENTION_WINDOW_SIZE] for idx in rand_idx])
     y = torch.stack([split[idx + 1:idx + ATTENTION_WINDOW_SIZE + 1] for idx in rand_idx])
     x, y = x.to(device), y.to(device)
     return x, y
 
-print(get_random_batch(train))
+def eval_model(model: nn.Module, x: Tensor, y_true: Tensor) -> dict[str, float]:
+    with torch.no_grad():
+        y_pred = model(x) # (batch size, window size, n embeding dims)
+        y_pred = y_pred.reshape(BATCH_SIZE * ATTENTION_WINDOW_SIZE, N_EMBEDING_DIMS) # (batch size * window size, n embeding dims)
+        y_true = y_true.reshape(BATCH_SIZE * ATTENTION_WINDOW_SIZE, N_EMBEDING_DIMS)
+        return {
+            "loss": F.cross_entropy(y_pred, y_true),
+            "accuracy": torch.argmax(y_pred, dim=1) == torch.argmax(y_true, dim=1),
+        }
 
-# def eval_model(model: nn.Module, )
+
+class MaskedAttentionHead(nn.Module):
+    def __init__(self, head_size:int, dropout_ratio: float=0):
+        """
+        ### Args:
+        head_size: number of dimensions for key, query and values vectors.
+
+        The forward call will project the tokens back to their embeding size.
+        """
+        super().__init__()
+        self.head_size = head_size
+        self.keys_projection = nn.Linear(N_EMBEDING_DIMS, head_size, bias=False)
+        self.queries_projection = nn.Linear(N_EMBEDING_DIMS, head_size, bias=False)
+        self.values_projection = nn.Linear(N_EMBEDING_DIMS, head_size, bias=False)
+        self.register_buffer('mask', torch.tril(torch.ones(ATTENTION_WINDOW_SIZE, ATTENTION_WINDOW_SIZE)))
+        self.dropout = nn.Dropout(dropout_ratio)
+        
+
+    def forward(self, x: Tensor) -> Tensor:
+        # x.shape = batch size, attention window size, embeding size
+        keys: Tensor = self.keys_projection(x)
+        queries = self.queries_projection(x)
+        values = self.values_projection(x)
+        weights = queries @ keys.swapaxes(1, 2)
+        weights /= sqrt(self.head_size)
+        weights = torch.masked_fill(weights, self.mask == 0, float('-inf'))
+        weights = self.dropout(weights)
+        print("weights:", weights.shape)
+        out = weights @ values
+
+        return out
+
+class GPT(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.token_embedding = nn.Embedding(vocab_len, N_EMBEDING_DIMS)
+        self.positional_embedding = nn.Embedding(ATTENTION_WINDOW_SIZE, N_EMBEDING_DIMS)
+        self.register_buffer("token_positions", torch.arange(ATTENTION_WINDOW_SIZE, device=device))
+        print("token_positions:", self.token_positions.shape)
+        self.attention_head = MaskedAttentionHead(1024)
+        self.post_head_projection = nn.Linear(1024, N_EMBEDING_DIMS)
+        self.un_embedding_layer = nn.Linear(N_EMBEDING_DIMS, vocab_len)
+
+    def forward(self, tokens_idx: Tensor) -> Tensor:
+        value_embedded_tokens = self.token_embedding(tokens_idx)
+        positional_embedded_tokens = self.positional_embedding(self.token_positions)
+        attended = self.attention_head(value_embedded_tokens + positional_embedded_tokens)
+        stream = self.post_head_projection(attended)
+        output_tokens_probabilities = self.un_embedding_layer(stream)
+        return output_tokens_probabilities
+
+    def generate(self, tokens_idx: Tensor, max_new_tokens: int):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # crop idx to the last block_size tokens
+            idx_cond = tokens_idx[:, -ATTENTION_WINDOW_SIZE:]
+            # get the predictions
+            logits, loss = self(idx_cond)
+            # focus only on the last time step
+            logits = logits[:, -1, :] # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1) # (B, C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            # append sampled index to the running sequence
+            tokens_idx = torch.cat((tokens_idx, idx_next), dim=1) # (B, T+1)
+        return tokens_idx
+
+model = GPT().to(device)
+
+print(sum(p.numel() for p in model.parameters())/1e6, 'mode parameters')
+optimizer = torch.optim.AdamW(model.parameters(), lr=LEARNING_RATE)
+
+
+for iter in range(N_TRAINING_STEPS):
+
+    # every once in a while evaluate the loss on train and val sets
+    if iter % LOGGING_INTERVAL == 0 or iter == N_TRAINING_STEPS - 1:
+        train_batch = get_random_batch(train)
+        train_metrics = eval_model(model, *train_batch)
+        print(f"step {iter}: train loss {train_metrics['loss']:.4f}, val loss {train_metrics['accuracy']:.4f}")
+        test_batch = get_random_batch(test)
+        test_metrics = eval_model(model, *test_batch)
+        print(f"step {iter}: train loss {train_metrics['loss']:.4f}, val loss {train_metrics['accuracy']:.4f}")
+
+    # sample a batch of data
+    xb, yb = get_random_batch('train')
+
+    # evaluate the loss
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+# generate from the model
+context = torch.zeros((1, 1), dtype=torch.long, device=device)
+print(decode(model.generate(context, max_new_tokens=500)[0].tolist()))
