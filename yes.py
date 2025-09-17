008f23e37e3d0bd90d1ba946f4a1db19e4835a83
Mauro Abidal Carrer <mauroabidal@yahoo.fr>
2025-09-17 14:18:36 +0000

    Feat: Added get_random_batch function.

--- yes.py
+++ yes.py
@@ -2,16 +2,18 @@ from functools import partial
 
 import torch
 from torch import nn
+from torch import Tensor
 from torch.nn import functional
 
 TEST_SPLIT_RATIO = 0.1
+ATTENTION_WINDOW_SIZE = 256
+BATCH_SIZE = 64
 
 
 # ! wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
 
 with open("input.txt", 'r', encoding='utf-8') as f:
     shakespeare_txt = f.read()
-
 
 vocab = sorted(list(set(shakespeare_txt)))
 vocab_len = len(vocab)
@@ -28,4 +30,16 @@ n_test_samples = int(TEST_SPLIT_RATIO * len(shakespeare_txt))
 train = dataset[:n_test_samples]
 test = dataset[n_test_samples:]
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
+
+def get_random_batch(split: Tensor) -> Tensor:
+    rand_idx = torch.randint(high=len(split), size=(BATCH_SIZE, ))
+    x = torch.stack([split[idx:idx + ATTENTION_WINDOW_SIZE] for idx in rand_idx])
+    y = torch.stack([split[idx + 1:idx + ATTENTION_WINDOW_SIZE + 1] for idx in rand_idx])
+    x, y = x.to(device), y.to(device)
+    return x, y
+
+print(get_random_batch(train))
+
+# def eval_model(model: nn.Module, )
